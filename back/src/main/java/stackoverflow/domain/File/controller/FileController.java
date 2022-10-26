092b68240504c5f8a103d1e17bdd76e1c2a36661
hipopotamus <qkqntjddn@gmail.com>
2022-10-26 20:18:58 +0900

    [Feat] File Upload Api 문서화

--- back/src/main/java/stackoverflow/domain/File/controller/FileController.java
+++ back/src/main/java/stackoverflow/domain/File/controller/FileController.java
@@ -3,6 +3,7 @@ package stackoverflow.domain.File.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import stackoverflow.domain.File.dto.FileDto;
@@ -13,7 +14,7 @@ import stackoverflow.global.common.dto.SingleResDto;
 public class FileController {
 
     @PostMapping("/file")
-    public ResponseEntity<SingleResDto<String>> uploadFile(FileDto fileDto) {
+    public ResponseEntity<SingleResDto<String>> uploadFile(@ModelAttribute FileDto fileDto) {
 
         SingleResDto<String> response =
                 new SingleResDto<>("https://travel-plan-repo-file.s3.ap-northeast-2.amazonaws.com/ProfileImg/00a76424-94a7-4683-aa0a-cbb0e683586b.jpeg");

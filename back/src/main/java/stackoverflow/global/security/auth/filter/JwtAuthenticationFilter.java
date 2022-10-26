626a39fd2561a17f0edbd6653238d2c28950fc46
philE22 <71927001+philE22@users.noreply.github.com>
2022-10-26 16:29:15 +0900

    [Feat] 인증 인가, auditable 추가 (#24)
    * 인증 인가, auditable 추가
    
    * auditable을 basetime으로 통합
    
    * AccountDetailsService추가, main클래스에 @JpaAuditing추가
    
    * application.yml 원복
    
    * push test
    
    * @Component 추가
    
    * account entity추가
    accountId -> id
    application.yml 파일에 jwt설정 추가
    roles -> role
    h2콘솔 설정 제거
    
    * ContextHolder에 principal이 dto로 추가되도록 변경

---
+++ back/src/main/java/stackoverflow/global/security/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,84 @@
+package stackoverflow.global.security.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import stackoverflow.domain.account.Account;
+import stackoverflow.global.security.auth.jwt.JwtTokenizer;
+import stackoverflow.global.security.auth.dto.LoginDto;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenizer jwtTokenizer;
+
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtTokenizer jwtTokenizer) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenizer = jwtTokenizer;
+    }
+
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        Account account = (Account) authResult.getPrincipal();
+
+        String accessToken = delegateAccessToken(account);
+        String refreshToken = delegateRefreshToken(account);
+
+        response.setHeader("Authorization", "Bearer " + accessToken);
+        response.setHeader("Refresh", refreshToken);
+
+        this.getSuccessHandler().onAuthenticationSuccess(request, response, authResult);
+    }
+
+    private String delegateAccessToken(Account account) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", account.getEmail());
+        claims.put("role", account.getRole());
+        claims.put("id", account.getId());   //claim에 id 넣는게 좋을까? subject 대신에
+
+        String subject = String.valueOf(account.getId());
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+
+    private String delegateRefreshToken(Account account) {
+        String subject = String.valueOf(account.getId());
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
+}
---
+++ back/src/main/java/stackoverflow/global/security/config/SecurityConfiguration.java
@@ -0,0 +1,93 @@
+package stackoverflow.global.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import stackoverflow.global.security.auth.handler.AccountAccessDeniedHandler;
+import stackoverflow.global.security.auth.handler.AccountAuthenticationEntryPoint;
+import stackoverflow.global.security.auth.jwt.JwtTokenizer;
+import stackoverflow.global.security.auth.filter.JwtVerificationFilter;
+import stackoverflow.global.security.auth.handler.AccountAuthenticationFailureHandler;
+import stackoverflow.global.security.auth.filter.JwtAuthenticationFilter;
+import stackoverflow.global.security.auth.utils.CustomAuthorityUtils;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.*;
+
+@Configuration
+public class SecurityConfiguration {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
+
+
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils) {
+        this.jwtTokenizer = jwtTokenizer;
+        this.authorityUtils = authorityUtils;
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()           //개발 환경 이후 설정해야할
+                .cors(withDefaults())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션 생성을 하지 않도록 설정
+                .and()
+                .formLogin().disable()      //Username과 Password를 JSON 형식으로 전송하는 방식을 사용할 것이므로 비활성화
+                .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new AccountAuthenticationEntryPoint())
+                .accessDeniedHandler(new AccountAccessDeniedHandler())
+                .and()
+                .apply(new CustomFilterConfigurer())
+                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()   //현재 모든 권한 열려 있음
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
+            jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(new AccountAuthenticationFailureHandler());
+
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
+
+            builder
+                    .addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+        }
+    }
+
+}

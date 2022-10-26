24ad104e27085333e70e1607539abba7e0a53a79
Minha <53070295+MinaRoh@users.noreply.github.com>
2022-10-26 12:14:46 +0900

    [Feat] tailwindCSS 초기설정

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <div className="App">hello world!</div>;
+  return (
+    <div className="text-3xl font-bold underline text-yellow-400">
+      Hello world!
+    </div>
+  );
 }
 
 export default App;

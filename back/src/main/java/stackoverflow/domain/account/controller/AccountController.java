623acf2aa233b8d28e3e8955ed0e53fde7fb034c
philE22 <moonwhlking@gmail.com>
2022-10-26 16:57:57 +0900

    initial commit

--- back/src/main/java/stackoverflow/domain/account/controller/AccountController.java
+++ back/src/main/java/stackoverflow/domain/account/controller/AccountController.java
@@ -1,4 +1,17 @@
 package stackoverflow.domain.account.controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountController {
+
+    @PostMapping("/account")
+    public ResponseEntity postAccount() {
+        return null;
+    }
 }
---
+++ back/src/main/java/stackoverflow/domain/account/dto/AccountResDto.java
@@ -0,0 +1,4 @@
+package stackoverflow.domain.account.dto;
+
+public class AccountResDto {
+}

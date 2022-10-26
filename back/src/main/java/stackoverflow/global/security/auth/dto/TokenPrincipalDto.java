804e6085d4c42d6c5947445fec8f2fde515316ff
philE22 <71927001+philE22@users.noreply.github.com>
2022-10-26 16:29:15 +0900

    [Feat] 인증 인가, auditable 추가 (#24)
    * 인증 인가, auditable 추가
    
    * auditable을 basetime으로 통합
    
    * AccountDetailsService추가, main클래스에 @JpaAuditing추가
    
    * application.yml 원복
    
    * push test
    
    * @Component 추가
    
    * account entity추가
    accountId -> id
    application.yml 파일에 jwt설정 추가
    roles -> role
    h2콘솔 설정 제거
    
    * ContextHolder에 principal이 dto로 추가되도록 변경

---
+++ back/src/main/java/stackoverflow/global/security/auth/dto/TokenPrincipalDto.java
@@ -0,0 +1,13 @@
+package stackoverflow.global.security.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TokenPrincipalDto {
+    private long id;
+    private String email;
+}

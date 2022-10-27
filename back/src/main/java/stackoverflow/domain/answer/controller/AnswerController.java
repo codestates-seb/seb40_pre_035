8c8e8ea30a1930d09728d1331c6926e499166dcf
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-10-27 14:15:00 +0900

    [BE] Answer Mock API 구현 (#4, #39)
    * Feat: Mock API 변경
     1. Controller의 반환객체는 PR 이후 SingleResDto객체 반환으로 변경하여 PR 예정
     2. String 반환되는 부분은 컨벤션에 맞춰 다시 PR 예정
    
    * fix:
    1. get answer 요청에 대한 answerdto 반환설정
    2. get answers 요청에 대해 mock객체는 10개로 변경. mockAccount id와 answer id값 다르게 변경
    3. dto에 있던 mockr객체 생성자 제거
    4. Entity클래스의 @GenerateValue 입력
    5. enxtity와 resDto에 BaseTime 상속
    6. answerService의 find메서드에 pageable 적용
    
    Co-authored-by: Dongjin Kim <“[vnyong9221@gmail.com>

---
+++ back/src/main/java/stackoverflow/domain/answer/controller/AnswerController.java
@@ -0,0 +1,85 @@
+package stackoverflow.domain.answer.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import stackoverflow.domain.account.dto.AnswerAccountResDto;
+import stackoverflow.domain.answer.dto.AnswerReqDto;
+import stackoverflow.domain.answer.dto.AnswerResDto;
+import stackoverflow.domain.answer.service.AnswerService;
+import stackoverflow.global.common.dto.PageDto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+@RestController
+public class AnswerController {
+    private AnswerService answerService;
+
+    @PostMapping
+    public ResponseEntity postAnswer(@RequestBody AnswerReqDto answerReqDto) {
+        String title = answerReqDto.getTitle();
+
+        return new ResponseEntity<>(String.format("POST request accepted. title: %s", title), HttpStatus.CREATED);
+    }
+
+
+    @PatchMapping("/{answerId}")
+    public ResponseEntity patchAnswer(@PathVariable("answerId") Long id,
+                                      @RequestBody AnswerReqDto answerReqDto) {
+
+        return new ResponseEntity<>(String.format("PATCH request accepted. ID: %d", id), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{answerId}")
+    public ResponseEntity getAnswer(@PathVariable("answerId") Long id) {
+
+        AnswerAccountResDto account = new AnswerAccountResDto();
+        account.setId(1L);
+        account.setEmail("account@gmail.com");
+        account.setPath("profile");
+        account.setNickname("nickname");
+        AnswerResDto answerResDto = new AnswerResDto(id, "title", "content",10, account);
+        answerResDto.setCreatedAt(LocalDateTime.now());
+        answerResDto.setModifiedAt(LocalDateTime.now());
+
+        return new ResponseEntity<>(answerResDto, HttpStatus.OK);
+    }
+
+
+    @GetMapping
+    public PageDto getAnswers(Pageable pageable) {
+        List<AnswerResDto> list = new ArrayList<>();
+
+        for(int i =1 ; i <=10 ; i++) {
+            AnswerAccountResDto account = new AnswerAccountResDto();
+            account.setId(100L+i);
+            account.setEmail("mock"+i+"@gmail.com");
+            account.setPath("profile"+i);
+            account.setNickname("nick"+i);
+            AnswerResDto answerResDto = new AnswerResDto(0L+i, "title"+i, "contents"+i, 2, account);
+            answerResDto.setCreatedAt(LocalDateTime.now());
+            answerResDto.setModifiedAt(LocalDateTime.now());
+
+            list.add(answerResDto);
+        }
+        Page<AnswerResDto> page = new PageImpl<>(list, pageable, 10);
+
+        return new PageDto<>(page);
+    }
+
+
+    @DeleteMapping("/{answerId}")
+    public ResponseEntity deleteAnswer(@PathVariable("answerId") Long id) {
+
+        return new ResponseEntity<>(String.format("DELETE request accepted. ID: %d",id), HttpStatus.OK);  /// 추후 NO_CONTENT로 변경해야함
+    }
+}
---
+++ back/src/main/java/stackoverflow/domain/answer/dto/AnswerResDto.java
@@ -0,0 +1,20 @@
+package stackoverflow.domain.answer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import stackoverflow.domain.account.dto.AnswerAccountResDto;
+import stackoverflow.domain.answer.entity.Answer;
+import stackoverflow.global.auditing.BaseTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AnswerResDto extends BaseTime {
+    private Long id;
+    private String title;
+    private String content;
+    private int totalVote;
+    private AnswerAccountResDto account;
+}

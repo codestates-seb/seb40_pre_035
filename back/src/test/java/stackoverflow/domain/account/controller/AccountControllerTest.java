91fda422dede5e980201447524368d25669522ae
philE22 <moonwhlking@gmail.com>
2022-10-28 11:30:43 +0900

    [Docs] initial commit

---
+++ back/src/test/java/stackoverflow/domain/account/controller/AccountControllerTest.java
@@ -0,0 +1,28 @@
+package stackoverflow.domain.account.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountControllerTest {
+
+    @Test
+    void accountAdd() {
+    }
+
+    @Test
+    void accountDetails() {
+    }
+
+    @Test
+    void accountModify() {
+    }
+
+    @Test
+    void accountRemove() {
+    }
+
+    @Test
+    void accountUserDetails() {
+    }
+}

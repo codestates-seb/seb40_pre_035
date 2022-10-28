939c781f5d6c16172ac2bebcb0c9f8e86cd23d82
Dongjin Kim <[vnyong9221@gmail.com>
2022-10-28 10:19:43 +0900

    Feat: modifyAnswer_Test, deleteAnswer_Test, getAnswer_Test

--- back/src/test/java/stackoverflow/domain/answer/controller/AnswerControllerTest.java
+++ back/src/test/java/stackoverflow/domain/answer/controller/AnswerControllerTest.java
@@ -13,17 +13,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import stackoverflow.domain.answer.dto.AnswerReqDto;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static stackoverflow.util.ApiDocumentUtils.getRequestPreProcessor;
 import static stackoverflow.util.ApiDocumentUtils.getResponsePreProcessor;
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -36,17 +38,17 @@ public class AnswerControllerTest {
     @Autowired
     private Gson gson;
 
+
+    //------------------------------------------------------------------------------------------------------
     @Test
     @DisplayName("Answer 생성_성공")
     public void createAnswer_Success_Test() throws Exception {
         //given
         String jwt = "AccessToken_Value";
-        String title = "testAnswerTitle";
         String content = "testAnswerContent";
         int totalVote = 0;
 
         AnswerReqDto answerReqDto = new AnswerReqDto();
-        answerReqDto.setTitle(title);
         answerReqDto.setContent(content);
         answerReqDto.setTotalVote(totalVote);
 
@@ -74,13 +76,12 @@ public class AnswerControllerTest {
                                 ),
                                 requestFields(
                                         List.of(
-                                                fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
                                                 fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
                                                 fieldWithPath("totalVote").type(JsonFieldType.NUMBER).description("총 투표")
                                         )
                                 ),
                                 responseFields(
-                                        List.of(
+                                        Arrays.asList(
                                                 fieldWithPath("data").type(JsonFieldType.STRING).description("Api 성공 메시지")
                                         )
                                 )
@@ -89,48 +90,192 @@ public class AnswerControllerTest {
     }
 
 
+    //------------------------------------------------------------------------------------------------------
     @Test
     @DisplayName("Answer 수정_성공")
     public void modifyQuestion_Success_Test() throws Exception {
         //given
+        String jwt = "AccessToken_Value";
+
+        Long answerId = 1L;
+        String content = "testAnswerContent";
+        int totalVote = 0;
+
+        AnswerReqDto answerReqDto = new AnswerReqDto();
+        answerReqDto.setContent(content);
+        answerReqDto.setTotalVote(totalVote);
+
+        String body = gson.toJson(answerReqDto);
 
         //when
+        ResultActions actions = mockMvc.perform(
+                patch("/answers/{answerId}", answerId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", jwt)
+                        .content(body)
+        );
 
         //then
-
+        actions
+                .andExpect(status().isOk())
+                .andDo(document("modifyAnswer",
+                                getRequestPreProcessor(),
+                                getResponsePreProcessor(),
+                                pathParameters(
+                                        parameterWithName("answerId").description("Answer 식별자")
+                                ),
+                                requestHeaders(
+                                        List.of(
+                                                headerWithName("Authorization").description("JWT")
+                                        )
+                                ),
+                                requestFields(
+                                        List.of(
+                                                fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
+                                                fieldWithPath("totalVote").type(JsonFieldType.NUMBER).description("총 투표")
+                                        )
+                                ),
+                                responseFields(
+                                        Arrays.asList(
+                                                fieldWithPath("data").type(JsonFieldType.STRING).description("Api 성공 메시지")
+                                        )
+                                )
+                        )
+                );
     }
 
+
+    //------------------------------------------------------------------------------------------------------
+    @Test
+    @DisplayName("Answer 조회_성공")
+    public void getAnswer_Success_Test() throws Exception {
+        //given
+        String jwt = "AccessToken_Value";
+        Long answerId = 1L;
+        String title = "testAnswerTitle";
+        String content = "testAnswerContent";
+        int totalVote = 0;
+
+        AnswerReqDto answerReqDto = new AnswerReqDto();
+        answerReqDto.setTitle(title);
+        answerReqDto.setContent(content);
+        answerReqDto.setTotalVote(totalVote);
+
+        String body = gson.toJson(answerReqDto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                get("/answers/{answerId}", answerId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", jwt)
+                        .content(body)
+        );
+
+        // then
+        actions
+                .andExpect(status().isOk())
+                .andDo(document("getAnswer",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("answerId").description("Answer 식별자")
+                        ),
+                        responseFields(
+                                Arrays.asList(
+                                        fieldWithPath("createAt").type(JsonFieldType.STRING).description("Answer 생성일자"),
+                                        fieldWithPath("modifiedAt").type(JsonFieldType.STRING).description("Answer 수정일자"),
+                                        fieldWithPath("answerId").type(JsonFieldType.NUMBER).description("Answer 식별자"),
+                                        fieldWithPath("content").type(JsonFieldType.STRING).description("Answer 내용"),
+                                        fieldWithPath("totalVote").type(JsonFieldType.NUMBER).description("Answer Vote"),
+                                        fieldWithPath("account").type(JsonFieldType.OBJECT).description("Answer 생성계정"),
+                                        fieldWithPath("account.id").type(JsonFieldType.NUMBER).description("Answer 생성계정 식별자"),
+                                        fieldWithPath("account.email").type(JsonFieldType.STRING).description("Answer 생성계정 email"),
+                                        fieldWithPath("account.path").type(JsonFieldType.STRING).description("Answer 생성계정 프로필 이미지 경로"),
+                                        fieldWithPath("account.nickname").type(JsonFieldType.STRING).description("Answer 생성계정 별칭")
+                                )
+                        )
+                ));
+    }
+
+
+    //------------------------------------------------------------------------------------------------------
+    @Test
+    @DisplayName("Answers 조회_성공")
+    public void getAnswers_Success_Test() throws Exception {
+        // given
+
+
+//        // when
+//        ResultActions actions =
+//                mockMvc.perform(
+//                        post("/answers")
+//                                .accept(MediaType.APPLICATION_JSON)
+//                                .contentType(MediaType.APPLICATION_JSON)
+//                                .content(content));
+//
+//        // then
+//        actions
+//                .andExpect(status().isOk())
+//
+//                .andDo(document("요청명_객체명",
+//                        getRequestPreProcessor(),
+//                        getResponsePreProcessor(),
+//                        pathParameters(
+//                                Arrays.asList(parameterWithName("answerId").description("식별자"))
+//                        ),
+//                        requestFields(
+//                                List.of(
+//                                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("식별자"),
+//                                        fieldWithPath("name").type(JsonFieldType.STRING).description("이름")
+//                                )
+//                        ),
+//                        responseFields(
+//                                Arrays.asList(
+//                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("데이터"),
+//                                        fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("식별자"),
+//                                        fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일")
+//                                )
+//                        )
+//                ));
+    }
+
+
+    //------------------------------------------------------------------------------------------------------
     @Test
     @DisplayName("Answer 삭제_성공")
     public void deleteAnswer_Success_Test() throws Exception {
         //given
+        String jwt = "AccessToken_Value";
+        Long answerId = 1L;
 
         //when
+        ResultActions actions = mockMvc.perform(
+                delete("/answers/{answerId}", answerId)
+                        .header("Authorization", jwt)
+        );
 
         //then
-
+        actions
+                .andExpect(status().isOk())
+                .andDo(document("deleteAnswer",
+                                getRequestPreProcessor(),
+                                getResponsePreProcessor(),
+                                pathParameters(
+                                        parameterWithName("answerId").description("Answer 식별자")
+                                ),
+                                requestHeaders(
+                                        List.of(
+                                                headerWithName("Authorization").description("JWT")
+                                        )
+                                ),
+                                responseFields(
+                                        Arrays.asList(
+                                                fieldWithPath("data").type(JsonFieldType.STRING).description("Api 성공 메시지")
+                                        )
+                                )
+                        )
+                );
     }
-
-    @Test
-    @DisplayName("Answer 삭제_성공")
-    public void getAnswer_Success_Test() throws Exception {
-        //given
-
-        //when
-
-        //then
-
-    }
-
-    @Test
-    @DisplayName("Answer 삭제_성공")
-    public void getAnswers_Success_Test() throws Exception {
-        //given
-
-        //when
-
-        //then
-
-    }
-
 }

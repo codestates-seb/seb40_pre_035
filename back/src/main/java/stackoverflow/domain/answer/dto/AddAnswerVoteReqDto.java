f57494c6d9c2a501e699bf55dafd1e67074d0d13
Dongjin Kim <[vnyong9221@gmail.com>
2022-10-28 15:16:14 +0900

    Feat: AnswerVote Test 코드 작성 Docs: AnswerVote adoc 항목 추가

--- back/src/main/java/stackoverflow/domain/answer/dto/AddAnswerVoteReqDto.java
+++ back/src/main/java/stackoverflow/domain/answer/dto/AddAnswerVoteReqDto.java
@@ -2,11 +2,12 @@ package stackoverflow.domain.answer.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import stackoverflow.global.common.enums.VoteState;
 
 @Getter
 @Setter
 public class AddAnswerVoteReqDto {
 
-    private String state;
+    private VoteState state;
 
 }

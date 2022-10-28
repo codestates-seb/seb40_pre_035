90ccf1c41edacc0031767b4c548c6faff829a2ef
Dongjin Kim <[vnyong9221@gmail.com>
2022-10-28 14:53:35 +0900

    Feat: AnswerVote 핸들러메서드 작성

--- back/src/main/java/stackoverflow/domain/answer/entity/AnswerVote.java
+++ back/src/main/java/stackoverflow/domain/answer/entity/AnswerVote.java
@@ -2,17 +2,20 @@ package stackoverflow.domain.answer.entity;
 
 import lombok.Data;
 import stackoverflow.domain.account.entity.Account;
+import stackoverflow.global.auditing.BaseTime;
+import stackoverflow.global.common.enums.VoteState;
 
 import javax.persistence.*;
 
 @Data
 @Entity
-public class AnswerVote {
+public class AnswerVote extends BaseTime {
     @Id @GeneratedValue
     @Column(name = "answerVote_id")
     private Long id;
 
-    private String state;
+    @Enumerated(EnumType.STRING)
+    private VoteState state;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id")

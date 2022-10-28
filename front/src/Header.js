6aace44a069a25a8e7f6c363702585d06c816a64
MinaRoh <00minha@gmail.com>
2022-10-28 13:52:48 +0900

    [Feat] 로그인시 상단메뉴바 디자인 추가

--- front/src/Header.js
+++ front/src/Header.js
@@ -1,6 +1,11 @@
-import { IconLogo, IconSearch } from '@stackoverflow/stacks-icons';
+import { IconLogo, IconSearch, IconPerson } from '@stackoverflow/stacks-icons';
 import ReactHtmlParser from 'react-html-parser';
+import { useState } from 'react';
 const Header = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const onClick = () => {
+    setIsLogin(!isLogin);
+  };
   return (
     <div className="flex-col">
       <div className="h-1 bg-primary-300"></div>
@@ -8,20 +13,44 @@ const Header = () => {
         <div className="items-center mx-2 my-1">
           {ReactHtmlParser(IconLogo)}
         </div>
+        <button className="hover:bg-soGray-light" onClick={onClick}>
+          임시 isLogin toggle
+        </button>
         <div className="flex items-center px-2 py-1 mx-2 mr-10 bg-white rounded-md grow">
           <div className="flex mx-2 my-1 text-soGray-icon">
             {ReactHtmlParser(IconSearch)}
           </div>
           <input type="text" placeholder="Search..."></input>
         </div>
-        <button className="px-3 py-1 mx-2 border rounded text-blue hover:bg-buttonSecondaryHover bg-buttonSecondary border-secondary-200">
-          Log in
-        </button>
-        <button className="px-2 py-1 mx-1 text-white border rounded hover:bg-buttonPrimaryHover bg-buttonPrimary border-secondary-300">
-          Sign up
-        </button>
+        <div>{isLogin ? <LoginGNB /> : <LogoutGNB />}</div>
       </div>
     </div>
+  );
+};
+
+const LoginGNB = () => {
+  return (
+    <div className="flex">
+      <button className="px-3 py-1 mx-2 text-gray hover:bg-soGray-light">
+        Logout
+      </button>
+      <div className="items-center p-2 hover:bg-soGray-light">
+        {ReactHtmlParser(IconPerson)}
+      </div>
+    </div>
+  );
+};
+
+const LogoutGNB = () => {
+  return (
+    <>
+      <button className="px-3 py-1 mx-2 border rounded text-blue hover:bg-buttonSecondaryHover bg-buttonSecondary border-secondary-200">
+        Log in
+      </button>
+      <button className="px-2 py-1 mx-1 text-white border rounded hover:bg-buttonPrimaryHover bg-buttonPrimary border-secondary-300">
+        Sign up
+      </button>
+    </>
   );
 };
 

2ede2460d508f0a0f39c53dbf153df2af2fc473b
Minha <53070295+MinaRoh@users.noreply.github.com>
2022-10-30 21:34:55 +0900

    [Feat] 로그인페이지 구현
    * [Feat] 로그인페이지 구현
    * [Fix] 로그인페이지 input type 변경

---
+++ front/src/pages/Login.js
@@ -0,0 +1,16 @@
+import { IconLogoGlyphMd } from '@stackoverflow/stacks-icons';
+import ReactHtmlParser from 'react-html-parser';
+import LoginInfo from '../components/login/LoginInfo';
+const Login = () => {
+  return (
+    <div className="flex justify-center h-screen align-center bg-soGray-bg">
+      <div className="flex-col justify-center w-1/4 mb-auto pt-14">
+        <div className="flex items-center justify-center mx-2 my-10">
+          {ReactHtmlParser(IconLogoGlyphMd)}
+        </div>
+        <LoginInfo />
+      </div>
+    </div>
+  );
+};
+export default Login;

b0cfaa9e25079e49a9941dea4f2c91ac678753e0
hipopotamus <qkqntjddn@gmail.com>
2022-10-31 11:03:49 +0900

    [Feat] Init Data 설정

---
+++ back/src/main/resources/static/db/data.sql
@@ -0,0 +1,35 @@
+INSERT INTO Account(account_id, createdAt, modifiedAt, email, nickname, password, profile, role) VALUES('1', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'test1@test.com', 'testNickname1', '{bcrypt}$2a$10$cBR6WPkscv66SOrXwOJ6jOH7akwby7R0p5reALf/cLhiNEDyPqnpa', 'test/path', 'USER');
+
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('101', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('102', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('103', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('104', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('105', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('106', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('107', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('108', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('109', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('110', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('111', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('112', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('113', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('114', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('115', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('116', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('117', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('118', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('119', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('120', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+INSERT INTO Question(question_id, createdAt, modifiedAt, content, title, account_id) VALUES('121', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testContenttestContenttestContenttestContenttestContent', 'testTitle', '1');
+
+INSERT INTO Answer(answer_id, createdAt, modifiedAt, content, totalVote, question_id) VALUES('1001', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testcontent', '10', '101');
+INSERT INTO Answer(answer_id, createdAt, modifiedAt, content, totalVote, question_id) VALUES('1002', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testcontent', '10', '101');
+INSERT INTO Answer(answer_id, createdAt, modifiedAt, content, totalVote, question_id) VALUES('1003', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testcontent', '10', '101');
+INSERT INTO Answer(answer_id, createdAt, modifiedAt, content, totalVote, question_id) VALUES('1004', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testcontent', '10', '101');
+INSERT INTO Answer(answer_id, createdAt, modifiedAt, content, totalVote, question_id) VALUES('1005', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'testcontent', '10', '101');
+
+insert into QuestionVote(questionVote_id, createdAt, modifiedAt, state, account_id, question_id) values('2001', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'UP', '1', '101');
+insert into QuestionVote(questionVote_id, createdAt, modifiedAt, state, account_id, question_id) values('2002', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'UP', '1', '101');
+insert into QuestionVote(questionVote_id, createdAt, modifiedAt, state, account_id, question_id) values('2003', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'DOWN', '1', '101');
+insert into QuestionVote(questionVote_id, createdAt, modifiedAt, state, account_id, question_id) values('2004', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'DOWN', '1', '101');
+insert into QuestionVote(questionVote_id, createdAt, modifiedAt, state, account_id, question_id) values('2005', '2022-10-14 17:13:10', '2022-10-14 17:13:10', 'DOWN', '1', '101');

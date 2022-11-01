ac8ef706a56d499eaf57197a66fb3447da816c6c
roxpray <si7531@naver.com>
2022-11-01 18:19:49 +0900

    api 연동

--- front/src/components/home/Main.js
+++ front/src/components/home/Main.js
@@ -2,18 +2,54 @@ import Pagination from './Pagination';
 import ListTop from './QuestionListTop';
 import ListMiddle from './QuestionListMiddle';
 import Card from './Card';
+import { useState, useEffect } from 'react';
+import { BASE_URL } from '../../util/api';
 
-const Mainbar = () => {
+function Mainbar({ questionId }) {
+  console.log('questionId');
+  const [questionList, setQuestionList] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/questions?page=1&size=10&sort=id%2Cdesc`)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error('could not fetch the data for that resource');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setIsPending(false);
+        setQuestionList(data.content);
+      })
+      //통신이 되면 json을 스테이츠로 변환
+      .catch((error) => {
+        setIsPending(false);
+        throw Error(error.message);
+      });
+  }, [questionId]);
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="flex flex-col">
-      <ListTop />
-      <div className="pt-10">
-        <ListMiddle />
+    <>
+      {console.log(questionList)}
+      <div className="flex flex-col">
+        <ListTop />
+        <div className="pt-10">
+          <ListMiddle />
+        </div>
+        <div className="questionList">
+          {questionList.map((card) => (
+            <Card key={card.id} item={card} />
+          ))}
+        </div>
+        <Pagination />
       </div>
-      <Card />
-      <Pagination />
-    </div>
+    </>
   );
-};
+}
 
 export default Mainbar;

ce9b06d6f93ab4b1fafd99034664c8eb800b06e4
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2022-11-01 23:04:24 +0900

    [Feat] 마이페이지 profile, questions API 연동

--- front/src/components/mypage_components/Answers.js
+++ front/src/components/mypage_components/Answers.js
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { BASE_URL } from '../../util/api';
 
 const Answers = () => {
   return (
--- front/src/components/mypage_components/Profile.js
+++ front/src/components/mypage_components/Profile.js
@@ -1,11 +1,31 @@
-function Profile() {
+import { useState, useEffect } from 'react';
+import { BASE_URL } from '../../util/api';
+
+const Profile = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/accounts/1`)
+      .then((res) => {
+        if (!res.ok) {
+          // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }, []);
+
   return (
     <div className="flex">
-      <div>
-        <img src="../../public/logo192.png" alt="" className="w-32 h-32" />
-      </div>
+      <img src={data?.profile} alt="" className="w-32 h-32" />
       <div className="self-center ml-3">
-        <div className="mx-2 mb-2 text-4xl font-bold ">김민아</div>
+        <div className="mx-2 mb-2 text-4xl font-bold ">{data?.nickname}</div>
         <ul className="flex">
           <li className="px-1.5">
             {/* <Link to="/">Logout</Link> */}
@@ -25,6 +45,6 @@ function Profile() {
       </div>
     </div>
   );
-}
+};
 
 export default Profile;

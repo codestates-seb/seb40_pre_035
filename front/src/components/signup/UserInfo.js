a4a3e55d3a9a73766eac67dbc0819e4f444742db
MinaRoh <00minha@gmail.com>
2022-11-01 18:08:09 +0900

    [Feat] 회원가입시 이메일 유효성 검사 추가
    - 이메일은 input type email로 제출시 브라우저에서 판단할것이라고 생각했지만 예외사항에 대비해 이메일 유효성 검사를 추가하였습니다.

--- front/src/components/signup/UserInfo.js
+++ front/src/components/signup/UserInfo.js
@@ -24,6 +24,12 @@ const UserInfo = () => {
       setUsernameError(true);
     } else setUsernameError(false);
   }
+  function checkEmail() {
+    const emailRegexp = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    if (!email || !emailRegexp.test(email)) {
+      setEmailError(true);
+    } else setEmailError(false);
+  }
   function checkPassword() {
     const passwordRegexp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
     if (!password || !passwordRegexp.test(password)) {
@@ -33,6 +39,7 @@ const UserInfo = () => {
 
   function onSubmit() {
     checkUsername();
+    checkEmail();
     checkPassword();
     if (!usernameError && !passwordError) return true;
     else return false;
@@ -46,7 +53,7 @@ const UserInfo = () => {
   return (
     <div className="flex-col w-1/3 my-5 ml-2 mr-10 align-middle justify-items-center">
       <div className="px-5 pt-3 pb-10 bg-white rounded-md drop-shadow-xl">
-        <form className="form">
+        <div className="form">
           <div className="flex-col justify-center mx-2 my-3">
             <div className="font-bold">Display name</div>
             <input
@@ -67,8 +74,11 @@ const UserInfo = () => {
               type="Email"
               value={email}
               onChange={handleEmail}
-              className="w-full px-2 py-1 border rounded border-soGray-light"
+              className={borderColor[emailError ?? false]}
             ></input>
+            <p className="text-xxs text-danger-500">
+              이메일 형식에 맞지 않습니다.
+            </p>
           </div>
           <div className="flex-col justify-center mx-2 my-3">
             <div className="font-bold">Password</div>
@@ -91,7 +101,7 @@ const UserInfo = () => {
           <button onClick={onSubmit} className="w-full mt-10 so-button-primary">
             Sign up
           </button>
-        </form>
+        </div>
       </div>
       <div className="flex justify-center my-5">
         <p className="mx-1">Already have an account?</p>

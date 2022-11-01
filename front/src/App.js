ce35e7dde635cb805cfeccb0e03223b55e7e4243
Mina Kim <rmaomina@gmail.com>
2022-11-01 16:19:27 +0900

    [Fix] 라우터 경로 수정 및 NotFound 스타일 수정

--- front/src/App.js
+++ front/src/App.js
@@ -7,6 +7,7 @@ import MyPage from './pages/MyPage';
 import Signup from './pages/Signup';
 import QuestionCreate from './pages/QuestionCreate';
 import QuestionDetail from './pages/QuestionDetail';
+import NotFound from './components/notfound/NotFound';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -18,10 +19,11 @@ function App() {
         <Route path="/mypage/*" element={<MyPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/questioncreate" element={<QuestionCreate />} />
-        <Route path="/questiondetail/:id" element={<QuestionDetail />} />
+        <Route path="/question" element={<Home />} />
+        <Route path="/question/create" element={<QuestionCreate />} />
+        <Route path="/question/detail/:id" element={<QuestionDetail />} />
         {/* 잘못된 주소로 접근한 경우 */}
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>

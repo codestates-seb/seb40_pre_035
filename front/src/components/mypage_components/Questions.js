9e8473cdddcb65a3e28d64a25996f47362aa1fd6
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2022-11-01 09:06:41 +0900

    [Feat] 마이페이지 css hover, focus 기능 추가

--- front/src/components/mypage_components/Questions.js
+++ front/src/components/mypage_components/Questions.js
@@ -2,16 +2,18 @@ function Questions() {
   return (
     <div className="w-full m-3">
       <p className="mb-2 text-2xl font-medium">0 Questions</p>
-      <div className="border rounded">
+      <div className="border rounded border-soGray-normal">
         <p className="p-12 m-2 text-center">
           You have not{' '}
-          <a href="http://localhost:3000/" className="text-blue-700">
+          <a href="http://localhost:3000/" className="text-secondary-500">
             asked
           </a>{' '}
           any questions
         </p>
-        <div className="p-4 text-blue-700 border-t">
-          <a href="http://localhost:3000/">Deleted questions</a>
+        <div className="p-3 border-t border-soGray-normal ">
+          <a href="http://localhost:3000/" className="text-secondary-500">
+            Deleted questions
+          </a>
         </div>
       </div>
     </div>

f6954c9f0c8383c8b627ae70ffaea6677b24b5d0
roxpray <si7531@naver.com>
2022-11-02 11:23:59 +0900

    [Feat][Fix] api연동 css 수정 라우팅

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header';
 import Footer from './components/footer/Footer';
+import QuestionMain from './pages/QuestionMain';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import MyPage from './pages/MyPage';
@@ -15,11 +16,13 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/questionMain" element={<QuestionMain />} />
         <Route path="/mypage/*" element={<MyPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/questioncreate" element={<QuestionCreate />} />
         <Route path="/questiondetail/:id" element={<QuestionDetail />} />
+
         {/* 잘못된 주소로 접근한 경우 */}
         <Route path="*" element={<Home />} />
       </Routes>

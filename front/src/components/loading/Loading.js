72f9fd220e41695193ac1b2751ce857dbf07548d
Mina Kim <rmaomina@gmail.com>
2022-11-03 11:58:50 +0900

    [Feat] 삭제 모달 화면 구현

--- front/src/components/loading/Loading.js
+++ front/src/components/loading/Loading.js
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="fixed flex items-center justify-center w-full mt-[100px]">
+    <div className="fixed inset-0 flex items-center justify-center">
       <img
         className="w-[250px] h-auto"
         src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921"

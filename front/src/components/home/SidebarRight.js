c50e2631cb2eb1769fe82b34c8cb77d2b7d1fa1b
Mina Kim <rmaomina@gmail.com>
2022-11-03 11:37:42 +0900

    [Refactor] Home 및 Sidebar 구조 변경 & 스타일 수정

--- front/src/components/home/SidebarRight.js
+++ front/src/components/home/SidebarRight.js
@@ -1,30 +1,27 @@
-/* eslint-disable jsx-a11y/alt-text */
+import { Link } from 'react-router-dom';
 import AD1 from '../../images/AD1.jpeg';
 import AD2 from '../../images/AD2.png';
 import AD3 from '../../images/AD3.png';
 
 export default function SidebarRight() {
   return (
-    // <div className="layout w-96 mt-5 ml-9">
-    <div className="flex flex-col h-screen p-4 inset-y-0 right-0 gap-1 bg-white shadow w-60 text-right">
+    <div className="flex-col p-4 text-right text-xxs">
       <div>
-        <a href="https://stackoverflow.com/">
-          <img src={AD1} />
-        </a>
-        <span className="text-xs text-buttonPrimary">Report this ad</span>
-      </div>
-
-      <div>
-        <a href="https://github.com/">
-          <img src={AD2} />
-        </a>
+        <Link to="https://stackoverflow.com/">
+          <img src={AD1} alt="AD1" />
+          <span className="text-xs text-buttonPrimary">Report this ad</span>
+        </Link>
       </div>
       <div>
-        <a href="https://www.codestates.com/?gclid=Cj0KCQjwwfiaBhC7ARIsAGvcPe5Zz-55ceMDlTGoFYgzP7V6P6rcdrmHYZgVf3I_wIeEg6Lea4FquxMaAtsKEALw_wcB">
-          <img src={AD3} />
-        </a>
+        <Link to="https://github.com/codestates-seb/seb40_pre_035">
+          <img src={AD2} alt="AD2" />
+        </Link>
+      </div>
+      <div>
+        <Link to="https://www.codestates.com/?gclid=Cj0KCQjwwfiaBhC7ARIsAGvcPe5Zz-55ceMDlTGoFYgzP7V6P6rcdrmHYZgVf3I_wIeEg6Lea4FquxMaAtsKEALw_wcB">
+          <img src={AD3} alt="AD3" />
+        </Link>
       </div>
     </div>
-    // </div>
   );
 }
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,16 +1,15 @@
-import SidebarHome from '../components/home/SidebarHome';
-import SidebarRight from '../components/home/SidebarRight';
-import QuestionAskBox from '../components/home/QuestionAskBox';
 import { useState, useEffect } from 'react';
-import { BASE_URL } from '../util/api';
+import { Link } from 'react-router-dom';
+import SidebarRight from '../components/home/SidebarRight';
+import Loading from '../components/loading/Loading';
 import Card from '../components/home/Card';
 
-function Home(questionId) {
+function Home() {
   const [questionList, setQuestionList] = useState([]);
   const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch(`${BASE_URL}/questions?page=1&size=10&sort=id%2Cdesc`)
+    fetch(`/questions?page=1&size=10&sort=id%2Cdesc`)
       .then((response) => {
         if (!response.ok) {
           throw Error('could not fetch the data for that resource');
@@ -21,50 +20,43 @@ function Home(questionId) {
         setIsPending(false);
         setQuestionList(data.content);
       })
-      //통신이 되면 json을 스테이츠로 변환
       .catch((error) => {
         setIsPending(false);
         throw Error(error.message);
       });
-  }, [questionId]);
+  }, []);
 
   if (isPending) {
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
   }
 
   return (
-    <>
-      <div className="content-around" />
-      <div className="relative flex h-auto mx-auto my-0 1-0-auto">
-        <SidebarHome />
-        <div className="flex p-10 leading-7 columns">
-          <div className="pl-8">
-            <div className="flex">
-              <div className="flex flex-col">
-                <div className="flex space-x-0">
-                  <div className="text-3xl pr-96">Top Questions</div>
-                  <div className="pl-5 mb-2">
-                    <QuestionAskBox />
-                  </div>
-                </div>
-                <div className="mt-16">
-                  {questionList && (
-                    <div className="questionList">
-                      {questionList.map((card) => (
-                        <Card key={card.id} item={card} />
-                      ))}
-                    </div>
-                  )}
-                </div>
-              </div>
+    <div className="flex flex-row flex-auto">
+      <div className="so-main-content">
+        <div className="flex flex-row items-center justify-between px-8 py-8">
+          <h2 className="text-xxl">Top Questions</h2>
+          <Link to={'/question/create'} className="so-button-primary">
+            Ask Question
+          </Link>
+        </div>
+        <div className="mr-8 border-t border-soGray-light">
+          {questionList && (
+            <div className="questionList">
+              {questionList.map((card) => (
+                <Card key={card.id} item={card} />
+              ))}
             </div>
-          </div>
-          <div className="cardLayout">
-            <div className="card"></div>
-          </div>
+          )}
         </div>
       </div>
-    </>
+      <aside className="w-[280px]">
+        <SidebarRight />
+      </aside>
+    </div>
   );
 }
 

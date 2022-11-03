847ad9592cfc8338216bba7a66d9a1d6f660ac88
Dongjin Kim <[vnyong9221@gmail.com>
2022-11-03 14:51:20 +0900

    Fix: 최신 생성 일자순 Answer 조회

--- back/src/main/java/stackoverflow/domain/answer/repository/AnswerRepository.java
+++ back/src/main/java/stackoverflow/domain/answer/repository/AnswerRepository.java
@@ -14,21 +14,25 @@ public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
     Optional<Answer> findById(Long id);
 
+    @EntityGraph(attributePaths = {"question", "account"})
+    @Query(value = "select answer from Answer answer Order by answer.createdAt desc")
     Page<Answer> findAllByOrderByIdDesc(Pageable pageable);
 
     @EntityGraph(attributePaths = {"question", "account"})
-    @Query(value = "select answer from Answer answer where answer.question.id = :questionId")
+    @Query(value = "select answer from Answer answer where answer.question.id = :questionId order by answer.createdAt desc")
     Page<Answer> findByQuestionWithAll(@Param("questionId") Long questionId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"question", "account"})
+    @Query(value = "select answer from Answer answer where answer.account.id = :accountId order by answer.createdAt desc")
+    Page<Answer> findByAccountWithAll(@Param("accountId") Long accountId, Pageable pageable);
 
     @EntityGraph(attributePaths = {"question", "account"})
     @Query(value = "select answer from Answer answer where answer.id = :answerId")
     Optional<Answer> findByIdWithAll(@Param("answerId") Long answerId);
 
+    @EntityGraph(attributePaths = {"question", "account"})
     @Query("select answer from Answer answer " +
             "join fetch answer.question question where answer.id = :answerId")
     Optional<Answer> findByIdWithQuestion(@Param("answerId") Long answerId);
-
-    @Query(value = "select answer from Answer answer where answer.account.id = :accountId")
-    Page<Answer> findByAccountWithAll(@Param("accountId") Long accountId, Pageable pageable);
 
 }

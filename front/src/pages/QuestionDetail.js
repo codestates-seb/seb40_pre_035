4afaa096ed0d04531f3400688bba102b3d54e006
Mina Kim <rmaomina@gmail.com>
2022-11-05 14:05:20 +0900

    [Feat] 질문 리스트, 단일 질문 조회 기능 수정

--- front/src/pages/QuestionDetail.js
+++ front/src/pages/QuestionDetail.js
@@ -1,18 +1,26 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import relTimeFormat from '../util/relativeTimeFormat';
-import Votes from '../components/questions/Votes';
-import AnswerList from '../components/answers/AnswerList';
-import QuestionDeleteModal from '../components/questions/QustionDeleteModal';
-import Loading from '../components/loading/Loading';
+import Sidebar from '../components/sidebar/Sidebar';
 import { Viewer } from '@toast-ui/react-editor';
+import { fetchQuestionDetail } from '../util/fetchQuestion';
+import { fetchAnswerList } from '../util/fetchAnswer';
+import QuestionDeleteModal from '../components/questions/QustionDeleteModal';
+import AnswerList from '../components/answers/AnswerList';
+import AnswerCreate from '../components/answers/AnswerCreate';
+import Votes from '../components/questions/Votes';
+import Loading from '../components/loading/Loading';
+import relTimeFormat from '../util/relativeTimeFormat';
+
 import '../components/common.css';
 
 function QuestionDetail() {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  const [data, setData] = useState(null);
+  const [info, setInfo] = useState({});
+  const [answerList, setAnswerList] = useState([]);
+  const [questionUpdate, setQuestionUpdate] = useState(true);
+  const [answerUpdate, setAnswerUpdate] = useState(true);
   const [isPending, setIsPending] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -29,22 +37,23 @@ function QuestionDetail() {
   };
 
   useEffect(() => {
-    fetch(`/questions/${id}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw Error('could not fetch the data for that resource');
-        }
-        return response.json();
-      })
-      .then((data) => {
+    if (questionUpdate || answerUpdate) {
+      fetchQuestionDetail(id).then((res) => {
+        console.log('question 리스트 조회');
         setIsPending(false);
-        setData(data);
-      })
-      .catch((error) => {
-        setIsPending(false);
-        throw Error(error.message);
+        setInfo(res);
+        setQuestionUpdate(false);
       });
-  }, []);
+
+      fetchAnswerList(id).then((res) => {
+        setIsPending(false);
+        setAnswerList(res.content);
+        console.log('answers 리스트 조회');
+        console.log(answerList);
+        setAnswerUpdate(false);
+      });
+    }
+  }, [questionUpdate, answerUpdate]);
 
   if (isPending) {
     return (
@@ -55,13 +64,16 @@ function QuestionDetail() {
   }
 
   return (
-    <div className="flex flex-row flex-auto">
-      {data && (
-        <div className="so-main-content max-w-none">
-          <div className="px-8 py-8 mb-3 border-b question-header border-soGray-light">
+    <div className="so-main-wrapper">
+      <nav className="sticky max-h-[calc(100vh-180px)] top-[60px] w-[164px] flex-grow-0 flex-shrink-0 basis-[164px]">
+        <Sidebar />
+      </nav>
+      {info && (
+        <div className="so-main-content max-w-none so-with-one-side">
+          <div className="px-8 pt-8 pb-4 mb-3 border-b question-header border-soGray-light">
             <div className="flex justify-between mb-4 question-title">
               <h2 className="row-auto pr-2 font-medium leading-tight break-words text-xxl">
-                {data.title}
+                {info.title}
               </h2>
               <div className="flex justify-end basis-52">
                 <button
@@ -73,35 +85,40 @@ function QuestionDetail() {
               </div>
             </div>
 
-            <div className="flex flex-row mb-4 text-sm user-info align-center">
+            <div className="flex flex-row text-sm user-info align-center">
               <span className="flex mr-4">
                 <img
-                  src={`${data.account.profile}`}
-                  alt={`${data.account.nickname}'s user avatar`}
+                  src={`${info.account.profile}`}
+                  alt={`${info.account.nickname}'s user avatar`}
                   width="16"
                   height="16"
                   className="mr-2"
                 />
                 <span className="text-soGray-darker">
-                  {data.account.nickname}
+                  {info.account.nickname}
                 </span>
               </span>
               <time className="mr-4 s-user-card--time">
                 <span className="mr-1 text-soGray-normal">Asked</span>
                 <span
                   className="text-soGray-darker"
-                  title={`${data.createdAt.split('T')[0]} ${
-                    data.createdAt.split('T')[1].split('.')[0]
+                  title={`${info.createdAt.split('T')[0]} ${
+                    info.createdAt.split('T')[1].split('.')[0]
                   }`}
                 >
-                  {relTimeFormat(data.createdAt)}
+                  {relTimeFormat(info.createdAt)}
                 </span>
               </time>
               <div>
-                <button onClick={onClickEdit} data-target={data.id}>
+                <button
+                  onClick={onClickEdit}
+                  data-target={info.id}
+                  className="mr-2"
+                >
                   Edit
                 </button>
-                <button onClick={showModalDelete} className="">
+                |
+                <button onClick={showModalDelete} className="ml-2">
                   Delete
                 </button>
               </div>
@@ -113,14 +130,22 @@ function QuestionDetail() {
               className="flex flex-row p-6"
             >
               <div className="flex justify-center mr-4 question-votes">
-                <Votes total={data.totalVote} />
+                <Votes
+                  total={info.totalVote}
+                  postId={info.id}
+                  updated={setQuestionUpdate}
+                  type="question"
+                />
               </div>
-              <div className="flex-auto question-content so-editor">
-                <Viewer initialValue={JSON.parse(data.content)} />
+              <div className="flex-auto question-content">
+                <Viewer initialValue={JSON.parse(info.content)} />
               </div>
             </div>
           </div>
-          <AnswerList questionId={data.id} />
+          {answerList && (
+            <AnswerList list={answerList} updated={setAnswerUpdate} />
+          )}
+          <AnswerCreate updated={setAnswerUpdate} questionId={info.id} />
         </div>
       )}
       {isModalOpen ? <QuestionDeleteModal /> : null}
--- front/src/pages/QuestionList.js
+++ front/src/pages/QuestionList.js
@@ -1,23 +1,71 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import SidebarRight from '../components/home/SidebarRight';
-import Mainbar from '../components/home/Main';
+import Loading from '../components/loading/Loading';
 import Sidebar from '../components/sidebar/Sidebar';
+import Card from '../components/home/Card';
+import Pagination from '../components/home/Pagination';
+import QuestionFilter from '../components/home/QuestionFilter';
+import { fetchQuestionList } from '../util/fetchQuestion';
 
-const QuestionList = () => {
+function QuestionList() {
+  const [questionList, setQuestionList] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+  const [currPage, setCurrPage] = useState(1);
+  const [pageInfo, setPageInfo] = useState({});
+  const [isUpdate, setIsUpdate] = useState(true);
+  const [currFilter, setCurrFilter] = useState('Newest');
+
+  useEffect(() => {
+    if (isUpdate) {
+      fetchQuestionList(currPage, currFilter).then((res) => {
+        setQuestionList(res.content);
+        setPageInfo(res);
+        setIsPending(false);
+        setIsUpdate(false);
+      });
+    }
+  }, [isUpdate]);
+
+  if (isPending) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  }
+
   return (
     <div className="so-main-wrapper">
       <nav className="sticky max-h-[calc(100vh-180px)] top-[60px] w-[164px] flex-grow-0 flex-shrink-0 basis-[164px]">
         <Sidebar />
       </nav>
-      <div className="so-main-content">
-        <div className="flex flex-row items-center justify-between px-8 py-8">
-          <h2 className="text-xxl">Top Questions</h2>
+      <div className="so-main-content so-with-both-side">
+        <div className="flex flex-row items-center justify-between px-6 py-8">
+          <h2 className="text-xxl">Questions</h2>
           <Link to="/question/create" className="so-button-primary">
             Ask Question
           </Link>
         </div>
+        <div>
+          <QuestionFilter />
+        </div>
         <div className="mr-8 border-t border-soGray-light">
-          <Mainbar />
+          {questionList && (
+            <div className="questionList">
+              {questionList.map((card) => (
+                <Card key={card.id} item={card} />
+              ))}
+            </div>
+          )}
+        </div>
+        <div>
+          <Pagination
+            currentPage={currPage}
+            pageInfo={pageInfo}
+            setCurrPage={setCurrPage}
+            setIsUpdate={setIsUpdate}
+          />
         </div>
       </div>
       <aside className="w-[280px] flex-grow-0 flex-shrink-0 basis-[280px]">
@@ -25,6 +73,6 @@ const QuestionList = () => {
       </aside>
     </div>
   );
-};
+}
 
 export default QuestionList;

28241d74f1bc28b850b601a2028c0b30f06f030f
Mina Kim <rmaomina@gmail.com>
2022-11-06 13:38:00 +0900

    [Fix] fetch 응답에 따라 Toast 메시지 적용

--- front/src/util/fetchAnswer.js
+++ front/src/util/fetchAnswer.js
@@ -1,22 +1,17 @@
 export const fetchAnswerList = async (id) => {
-  return (
-    fetch(`/answers/question/${id}?size=100`)
-      // /answers/question/46?size=100
-      .then((response) => {
-        if (!response.ok) {
-          console.log(response.statusText);
-          throw Error('could not fetch the answer data.');
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        throw Error(error.message);
-      })
-  );
+  return fetch(`/answers/question/${id}?size=100`)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error('유효하지 않은 요청입니다.');
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw Error(error.message);
+    });
 };
 
 export const fetchCreateAnswer = async (fetchData) => {
-  console.log(fetchData);
   return fetch(`/answers`, {
     method: 'POST',
     headers: {
@@ -26,9 +21,7 @@ export const fetchCreateAnswer = async (fetchData) => {
     body: JSON.stringify(fetchData),
   })
     .then((response) => {
-      console.log(response);
       if (!response.ok) {
-        console.log(response.statusText);
         throw Error('유효하지 않은 요청입니다.');
       }
       return response.json();
@@ -46,7 +39,6 @@ export const fetchDeleteAnswer = async (id) => {
     },
   })
     .then((response) => {
-      console.log(response);
       if (!response.ok) {
         throw Error('유효하지 않은 요청입니다.');
       }

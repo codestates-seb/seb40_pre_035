64b464bfb48b0251a664966f10feb445672f6f4c
MinaRoh <00minha@gmail.com>
2022-11-06 11:47:33 +0900

    [Feat] 로그인/회원가입시 오류 toast 메시지 추가

--- front/src/util/fetchLogin.js
+++ front/src/util/fetchLogin.js
@@ -1,7 +1,8 @@
 export const BASE_URL = 'http://13.125.238.70:8080'; // 다른페이지에서 아직 참조중이라 남겨둡니다
+import { showToast } from '../components/toast/Toast';
 
 export const fetchLogin = async (data) => {
-  fetch(`/auth/login`, {
+  return fetch(`/auth/login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
@@ -11,22 +12,20 @@ export const fetchLogin = async (data) => {
   })
     .then((res) => {
       if (!res.ok) {
+        console.log(res);
+        if (res.status === 401) showToast('Wrong Email or Password');
         throw Error('could not fetch the data for that resource');
       }
+      if (res.status === 200) {
+        showToast('Login Success!');
 
-      // 토큰 저장
-      const accessToken = res.headers.get('Authorization');
-      const refreshToken = res.headers.get('refresh');
-      sessionStorage.setItem('access_token', accessToken); // 30분
-      sessionStorage.setItem('refresh_token', refreshToken); // 420분
-
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data.data);
-      console.log('로그인 되었습니다.');
-      alert('로그인되었습니다.');
-      window.location.reload();
+        // 토큰 저장
+        const accessToken = res.headers.get('Authorization');
+        const refreshToken = res.headers.get('refresh');
+        sessionStorage.setItem('access_token', accessToken); // 30분
+        sessionStorage.setItem('refresh_token', refreshToken); // 420분
+      }
+      return res;
     })
     .catch((err) => {
       console.error(err.message);
@@ -35,33 +34,22 @@ export const fetchLogin = async (data) => {
 
 // 로그인한 Account 조회
 export const fetchUserInfo = async () => {
-  return (
-    fetch(`/accounts/user`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        Accept: 'application/json',
-        authorization: sessionStorage.getItem('access_token'),
-      },
+  return fetch(`/accounts/user`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+      Accept: 'application/json',
+      authorization: sessionStorage.getItem('access_token'),
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        // error coming back from server
+        throw Error('could not fetch the data for that resource');
+      }
+      return res.json();
     })
-      .then((res) => {
-        if (!res.ok) {
-          // error coming back from server
-          throw Error('could not fetch the data for that resource');
-        }
-        return res.json();
-      })
-      // .then((data) => {
-      //   console.log(data);
-      //   let userdata = {
-      //     email: data.email,
-      //     username: data.nickname,
-      //     profile: data.profile,
-      //   };
-      //   return userdata;
-      // })
-      .catch((err) => {
-        console.error(err.message);
-      })
-  );
+    .catch((err) => {
+      console.error(err.message);
+    });
 };

e0b194e68d7a17f2c778f4b7762903993caf53cc
Mina Kim <rmaomina@gmail.com>
2022-11-06 13:43:58 +0900

    [Feat] 답변 생성 기능 구현

--- front/src/components/answers/AnswerCreate.js
+++ front/src/components/answers/AnswerCreate.js
@@ -1,34 +1,43 @@
 import { useState } from 'react';
-import Editor from '../editor/Editor';
+import { Editor } from '../editor/Editor';
+import { checkIfLogined } from '../../util/fetchLogin';
 import { fetchCreateAnswer } from '../../util/fetchAnswer';
 import { showToast } from '../toast/Toast';
 
 function AnswerCreate({ questionId, updated }) {
   const [content, setContent] = useState('');
+  const [isEditorClear, setIsEditorClear] = useState(false);
 
   const onChangeContent = (content) => {
     setContent(content);
   };
 
-  const onClickSubmit = () => {
+  const onClickSubmit = async () => {
     if (content.length < 50) {
       showToast('Minimum 50 characters.');
     } else {
-      fetchCreateAnswer({ questionId, content }).then(() => {
-        updated(true);
+      checkIfLogined().then(() => {
+        fetchCreateAnswer({ questionId, content }).then(() => {
+          setIsEditorClear(true);
+          updated(true);
+        });
       });
     }
   };
 
   return (
     <div className="p-4">
-      <Editor onChange={onChangeContent} height={'300px'} />
+      <Editor
+        onChange={onChangeContent}
+        height={'300px'}
+        isEditorClear={isEditorClear}
+        setIsEditorClear={setIsEditorClear}
+      />
       <div className="flex">
         <button
           className="mr-3 so-button-primary"
           type="button"
           autoComplete="off"
-          disabled=""
           onClick={onClickSubmit}
         >
           Post your answer
